58964536db3ea048d4c303268355351262f47be3
valvenom <valentin.venom@protonmail.com>
2021-12-04 15:57:04 +0100

    Corrected typo

---
+++ src/main/java/fr/eql/al36/spring/projet/eqlexchange/repository/CurrencyTypeRepository.java
@@ -0,0 +1,8 @@
+package fr.eql.al36.spring.projet.eqlexchange.repository;
+
+import fr.eql.al36.spring.projet.eqlexchange.domain.CurrencyType;
+import fr.eql.al36.spring.projet.eqlexchange.domain.Payment;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CurrencyTypeRepository extends CrudRepository<CurrencyType, Integer> {
+}
