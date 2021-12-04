2a8267e3f8e6609d6ad68fcfb0f5da44083cb827
valvenom <valentin.venom@protonmail.com>
2021-12-04 15:52:45 +0100

    Added username attribute

--- src/main/java/fr/eql/al36/spring/projet/eqlexchange/domain/User.java
+++ src/main/java/fr/eql/al36/spring/projet/eqlexchange/domain/User.java
@@ -17,6 +17,7 @@ public class User {
 
     private String firstName;
     private String lastName;
+    private String username;
     private LocalDate dateOfBirth;
     private String email;
     private String password;
