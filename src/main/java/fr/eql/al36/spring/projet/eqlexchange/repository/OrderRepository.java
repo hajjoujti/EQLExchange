d941d79fac5df2738bb5331b75480ade9d2068fc
valvenom <valentin.venom@protonmail.com>
2021-12-04 15:58:27 +0100

    Corrected typo

--- src/main/java/fr/eql/al36/spring/projet/eqlexchange/repository/OrderRepository.java
+++ src/main/java/fr/eql/al36/spring/projet/eqlexchange/repository/OrderRepository.java
@@ -4,4 +4,5 @@ import fr.eql.al36.spring.projet.eqlexchange.domain.Order;
 import org.springframework.data.repository.CrudRepository;
 
 public interface OrderRepository extends CrudRepository<Order, Integer> {
+
 }
