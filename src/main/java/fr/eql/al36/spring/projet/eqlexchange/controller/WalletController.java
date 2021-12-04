c5e40ad8cebd9444b2ab495452525d6270d18ca9
valvenom <valentin.venom@protonmail.com>
2021-12-04 21:09:35 +0100

    Created controller methods for index, wallet and asset details page

---
+++ src/main/java/fr/eql/al36/spring/projet/eqlexchange/controller/WalletController.java
@@ -0,0 +1,44 @@
+package fr.eql.al36.spring.projet.eqlexchange.controller;
+
+import fr.eql.al36.spring.projet.eqlexchange.repository.AssetRepository;
+import fr.eql.al36.spring.projet.eqlexchange.repository.OrderRepository;
+import fr.eql.al36.spring.projet.eqlexchange.repository.TransactionRepository;
+import fr.eql.al36.spring.projet.eqlexchange.repository.UserRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class WalletController {
+
+    private final AssetRepository assetRepository;
+    private final OrderRepository orderRepository;
+    private final TransactionRepository transactionRepository;
+    private final UserRepository userRepository;
+
+    public WalletController(AssetRepository assetRepository, OrderRepository orderRepository, TransactionRepository transactionRepository, UserRepository userRepository) {
+        this.assetRepository = assetRepository;
+        this.orderRepository = orderRepository;
+        this.transactionRepository = transactionRepository;
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping("/")
+    public String getIndexPage(Model model) {
+        return "index";
+    }
+
+    @GetMapping("wallet/show")
+    public String displayWallet(Model model) {
+        model.addAttribute("assets", assetRepository.getAssetsByUserOrderByBalanceDesc(userRepository.findById(2).get()));
+        return "wallet/show";
+    }
+
+    @GetMapping("wallet/{id}")
+    public String displayAsset(Model model, @PathVariable String id) {
+        model.addAttribute("asset", assetRepository.findById(Integer.parseInt(id)).get());
+        model.addAttribute(transactionRepository.findById(id).get());
+        return "wallet/details";
+    }
+}
