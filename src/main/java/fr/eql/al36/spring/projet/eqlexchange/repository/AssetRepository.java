507c52fe03b42dbde65e4c20ff5f6df82335befb
valvenom <valentin.venom@protonmail.com>
2021-12-04 15:59:19 +0100

    Created method for wallet display

--- src/main/java/fr/eql/al36/spring/projet/eqlexchange/repository/AssetRepository.java
+++ src/main/java/fr/eql/al36/spring/projet/eqlexchange/repository/AssetRepository.java
@@ -1,7 +1,12 @@
 package fr.eql.al36.spring.projet.eqlexchange.repository;
 
 import fr.eql.al36.spring.projet.eqlexchange.domain.Asset;
+import fr.eql.al36.spring.projet.eqlexchange.domain.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface AssetRepository extends CrudRepository<Asset, Integer> {
+
+    List<Asset> getAssetByUserOrderByBalanceDesc(User user);
 }
