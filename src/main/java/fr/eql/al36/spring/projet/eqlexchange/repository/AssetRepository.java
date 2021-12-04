3e8e09d2ef847689c54e943d766575bdfe98a9b0
valvenom <valentin.venom@protonmail.com>
2021-12-04 21:07:21 +0100

    Created method to get user assets

--- src/main/java/fr/eql/al36/spring/projet/eqlexchange/repository/AssetRepository.java
+++ src/main/java/fr/eql/al36/spring/projet/eqlexchange/repository/AssetRepository.java
@@ -8,5 +8,5 @@ import java.util.List;
 
 public interface AssetRepository extends CrudRepository<Asset, Integer> {
 
-    List<Asset> getAssetByUserOrderByBalanceDesc(User user);
+    List<Asset> getAssetsByUserOrderByBalanceDesc(User user);
 }
