20228a4addb569cbc6523758d4c7ea4f925665ed
valvenom <valentin.venom@protonmail.com>
2021-12-04 15:50:23 +0100

    Corrected name change

--- src/main/java/fr/eql/al36/spring/projet/eqlexchange/EQLExchangeApplication.java
+++ src/main/java/fr/eql/al36/spring/projet/eqlexchange/EQLExchangeApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EqlcoinApplication {
+public class EQLExchangeApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EqlcoinApplication.class, args);
+        SpringApplication.run(EQLExchangeApplication.class, args);
     }
 
 }
