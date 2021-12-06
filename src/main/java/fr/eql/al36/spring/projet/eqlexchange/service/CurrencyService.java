2c24e05cb98165d8c370ffb896386df39a7c3e1c
hajjoujti <80820935+hajjoujti@users.noreply.github.com>
2021-12-06 17:16:01 +0100

    created CurrencyService and CurrencyController

---
+++ src/main/java/fr/eql/al36/spring/projet/eqlexchange/service/CurrencyService.java
@@ -0,0 +1,27 @@
+package fr.eql.al36.spring.projet.eqlexchange.service;
+
+import fr.eql.al36.spring.projet.eqlexchange.domain.Currency;
+import fr.eql.al36.spring.projet.eqlexchange.repository.CurrencyRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CurrencyService {
+
+    private final CurrencyRepository currencyRepository;
+
+
+    public CurrencyService(CurrencyRepository currencyRepository) {
+        this.currencyRepository = currencyRepository;
+    }
+
+
+    public Currency findCurrencyById(Integer id) {
+        if(currencyRepository.findById(id).isPresent()) {
+            return currencyRepository.findById(id).get();
+        }
+        return null;
+    }
+
+}
