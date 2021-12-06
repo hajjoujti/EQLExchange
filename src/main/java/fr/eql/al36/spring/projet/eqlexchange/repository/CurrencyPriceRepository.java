b5d1e0cd829f3109ceccb1ec008137fd2fda81b2
hajjoujti <80820935+hajjoujti@users.noreply.github.com>
2021-12-06 23:19:55 +0100

    created CurrencyPriceRepository and findTopByCurrencyOrderByIdDesc and getAllByCurrencyOrderById methods

---
+++ src/main/java/fr/eql/al36/spring/projet/eqlexchange/repository/CurrencyPriceRepository.java
@@ -0,0 +1,15 @@
+package fr.eql.al36.spring.projet.eqlexchange.repository;
+
+import fr.eql.al36.spring.projet.eqlexchange.domain.Currency;
+import fr.eql.al36.spring.projet.eqlexchange.domain.CurrencyPrice;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface CurrencyPriceRepository extends CrudRepository<CurrencyPrice, Integer> {
+
+    CurrencyPrice findTopByCurrencyOrderByIdDesc(Currency currency);
+
+    List<CurrencyPrice> getAllByCurrencyOrderById(Currency currency);
+
+}
