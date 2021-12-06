60d88414e7cd758df2f8dae4bb54ef01081bead4
hajjoujti <80820935+hajjoujti@users.noreply.github.com>
2021-12-06 15:33:40 +0100

    created getAllByAssetIn query method

--- src/main/java/fr/eql/al36/spring/projet/eqlexchange/repository/TradeOrderRepository.java
+++ src/main/java/fr/eql/al36/spring/projet/eqlexchange/repository/TradeOrderRepository.java
@@ -1,8 +1,13 @@
 package fr.eql.al36.spring.projet.eqlexchange.repository;
 
+import fr.eql.al36.spring.projet.eqlexchange.domain.Asset;
 import fr.eql.al36.spring.projet.eqlexchange.domain.TradeOrder;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface TradeOrderRepository extends CrudRepository<TradeOrder, Integer> {
+
+    List<TradeOrder> getAllByAssetIn(List<Asset> assets);
 
 }
