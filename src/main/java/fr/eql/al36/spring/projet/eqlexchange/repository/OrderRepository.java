c2310e59eaf48f1b72595b25b1d2e787f2127177
valvenom <valentin.venom@protonmail.com>
2021-12-06 10:13:06 +0100

    Renamed Order class to TradeOrder

--- src/main/java/fr/eql/al36/spring/projet/eqlexchange/repository/OrderRepository.java
+++
@@ -1,8 +0,0 @@
-package fr.eql.al36.spring.projet.eqlexchange.repository;
-
-import fr.eql.al36.spring.projet.eqlexchange.domain.Order;
-import org.springframework.data.repository.CrudRepository;
-
-public interface OrderRepository extends CrudRepository<Order, Integer> {
-
-}
