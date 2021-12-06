68d265803392e4c2dff0f6028f131785894778bf
valvenom <valentin.venom@protonmail.com>
2021-12-06 23:01:02 +0100

    Remapped transaction/show-all to wallet/history

--- src/main/java/fr/eql/al36/spring/projet/eqlexchange/controller/TransactionController.java
+++ src/main/java/fr/eql/al36/spring/projet/eqlexchange/controller/TransactionController.java
@@ -19,11 +19,11 @@ public class TransactionController {
     }
 
 
-    @GetMapping("transaction/show-all")
+    @GetMapping("wallet/history")
     public String displayWallet(Model model, HttpSession session) {
         User connectedUser = (User) session.getAttribute("sessionUser");
         model.addAttribute("transactions", transactionService.getTransactionsDoneByUser(connectedUser));
-        return "transaction/show-all";
+        return "wallet/history";
     }
 
 }
