90086132271e7b47ecfe01c0a87ae2672385ba23
hajjoujti <80820935+hajjoujti@users.noreply.github.com>
2021-12-06 15:36:50 +0100

    created TransactionController and TransactionService

---
+++ src/main/java/fr/eql/al36/spring/projet/eqlexchange/controller/TransactionController.java
@@ -0,0 +1,29 @@
+package fr.eql.al36.spring.projet.eqlexchange.controller;
+
+import fr.eql.al36.spring.projet.eqlexchange.domain.User;
+import fr.eql.al36.spring.projet.eqlexchange.service.TransactionService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+public class TransactionController {
+
+    private final TransactionService transactionService;
+
+
+    public TransactionController(TransactionService transactionService) {
+        this.transactionService = transactionService;
+    }
+
+
+    @GetMapping("transaction/show-all")
+    public String displayWallet(Model model, HttpSession session) {
+        User connectedUser = (User) session.getAttribute("sessionUser");
+        model.addAttribute("transactions", transactionService.getTransactionsDoneByUser(connectedUser));
+        return "transaction/show-all";
+    }
+
+}
---
+++ src/main/java/fr/eql/al36/spring/projet/eqlexchange/service/TransactionService.java
@@ -0,0 +1,38 @@
+package fr.eql.al36.spring.projet.eqlexchange.service;
+
+import fr.eql.al36.spring.projet.eqlexchange.domain.Asset;
+import fr.eql.al36.spring.projet.eqlexchange.domain.TradeOrder;
+import fr.eql.al36.spring.projet.eqlexchange.domain.Transaction;
+import fr.eql.al36.spring.projet.eqlexchange.domain.User;
+import fr.eql.al36.spring.projet.eqlexchange.repository.AssetRepository;
+import fr.eql.al36.spring.projet.eqlexchange.repository.TradeOrderRepository;
+import fr.eql.al36.spring.projet.eqlexchange.repository.TransactionRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TransactionService {
+
+    private final TransactionRepository transactionRepository;
+
+    private final TradeOrderRepository tradeOrderRepository;
+
+    private final AssetRepository assetRepository;
+
+
+    public TransactionService(TransactionRepository transactionRepository, TradeOrderRepository tradeOrderRepository,
+                              AssetRepository assetRepository) {
+        this.transactionRepository = transactionRepository;
+        this.tradeOrderRepository = tradeOrderRepository;
+        this.assetRepository = assetRepository;
+    }
+
+
+    public List<Transaction> getTransactionsDoneByUser(User user) {
+        List<Asset> assets = assetRepository.getAllByUser(user);
+        List<TradeOrder> tradeOrders = tradeOrderRepository.getAllByAssetIn(assets);
+        return transactionRepository.findAllByTradeOrders(tradeOrders);
+    }
+
+}
