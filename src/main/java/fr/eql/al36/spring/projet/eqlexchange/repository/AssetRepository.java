99bcea4109ac5c574f8a474bca079a7b26a91ee3
valvenom <valentin.venom@protonmail.com>
2021-12-06 13:41:21 +0100

    Added getAssetByUserAndCurrency method

--- src/main/java/fr/eql/al36/spring/projet/eqlexchange/repository/AssetRepository.java
+++ src/main/java/fr/eql/al36/spring/projet/eqlexchange/repository/AssetRepository.java
@@ -1,12 +1,18 @@
 package fr.eql.al36.spring.projet.eqlexchange.repository;
 
 import fr.eql.al36.spring.projet.eqlexchange.domain.Asset;
+import fr.eql.al36.spring.projet.eqlexchange.domain.Currency;
 import fr.eql.al36.spring.projet.eqlexchange.domain.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface AssetRepository extends CrudRepository<Asset, Integer> {
 
     List<Asset> getAssetsByUserOrderByBalanceDesc(User user);
+
+    @Query("SELECT a from Asset a WHERE a.user = :user AND a.currency = :currency")
+    Asset getAssetByUserAndCurrency(@Param("user") User user, @Param("currency") Currency currency);
 }
