8b7fb5ed56053e108e03e5d09f57e4213d4ab06c
valvenom <valentin.venom@protonmail.com>
2021-12-07 12:15:26 +0100

    Called AssetService in WalletController

--- src/main/java/fr/eql/al36/spring/projet/eqlexchange/controller/WalletController.java
+++ src/main/java/fr/eql/al36/spring/projet/eqlexchange/controller/WalletController.java
@@ -1,10 +1,11 @@
 package fr.eql.al36.spring.projet.eqlexchange.controller;
 
 import fr.eql.al36.spring.projet.eqlexchange.domain.User;
-import fr.eql.al36.spring.projet.eqlexchange.repository.AssetRepository;
 import fr.eql.al36.spring.projet.eqlexchange.repository.TradeOrderRepository;
 import fr.eql.al36.spring.projet.eqlexchange.repository.TransactionRepository;
 import fr.eql.al36.spring.projet.eqlexchange.repository.UserRepository;
+import fr.eql.al36.spring.projet.eqlexchange.service.AssetService;
+import fr.eql.al36.spring.projet.eqlexchange.service.CurrencyPriceService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,16 +16,18 @@ import javax.servlet.http.HttpSession;
 @Controller
 public class WalletController {
 
-    private final AssetRepository assetRepository;
+    private final AssetService assetService;
     private final TradeOrderRepository tradeOrderRepository;
     private final TransactionRepository transactionRepository;
     private final UserRepository userRepository;
+    private final CurrencyPriceService currencyPriceService;
 
-    public WalletController(AssetRepository assetRepository, TradeOrderRepository tradeOrderRepository, TransactionRepository transactionRepository, UserRepository userRepository) {
-        this.assetRepository = assetRepository;
+    public WalletController(AssetService assetService, TradeOrderRepository tradeOrderRepository, TransactionRepository transactionRepository, UserRepository userRepository, CurrencyPriceService currencyPriceService) {
+        this.assetService = assetService;
         this.tradeOrderRepository = tradeOrderRepository;
         this.transactionRepository = transactionRepository;
         this.userRepository = userRepository;
+        this.currencyPriceService = currencyPriceService;
     }
 
     @GetMapping("/")
@@ -35,14 +38,14 @@ public class WalletController {
     @GetMapping("wallet")
     public String displayWallet(Model model, HttpSession session) {
         User connectedUser = (User) session.getAttribute("sessionUser");
-        model.addAttribute("assets", assetRepository.getAssetsByUserOrderByBalanceDesc(connectedUser));
+        model.addAttribute("assets", assetService.getUserWallet(connectedUser));
         return "wallet/show";
     }
 
     @GetMapping("wallet/{id}")
     public String displayAsset(Model model, HttpSession session, @PathVariable String id) {
         User connectedUser = (User) session.getAttribute("sessionUser");
-        model.addAttribute("asset", assetRepository.findById(Integer.parseInt(id)).get());
+        model.addAttribute("asset", assetService.getById(Integer.parseInt(id)));
         return "wallet/details";
     }
 }
--- src/main/java/fr/eql/al36/spring/projet/eqlexchange/service/AssetService.java
+++ src/main/java/fr/eql/al36/spring/projet/eqlexchange/service/AssetService.java
@@ -1,18 +1,43 @@
 package fr.eql.al36.spring.projet.eqlexchange.service;
 
 import fr.eql.al36.spring.projet.eqlexchange.domain.Asset;
+import fr.eql.al36.spring.projet.eqlexchange.domain.Currency;
+import fr.eql.al36.spring.projet.eqlexchange.domain.User;
 import fr.eql.al36.spring.projet.eqlexchange.repository.AssetRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class AssetService {
 
     private AssetRepository assetRepository;
+    private CurrencyService currencyService;
 
     public void creditFromSourceAsset(Asset targetAsset, Asset sourceAsset, double amount) {
         if (targetAsset.getCurrency() == sourceAsset.getCurrency() && sourceAsset.getBalance() >= amount) {
             sourceAsset.setBalance(sourceAsset.getBalance() - amount);
             targetAsset.setBalance(targetAsset.getBalance() + amount);
         }
+    }
+
+    public List<Asset> getUserWallet(User user) {
+        return assetRepository.getAssetsByUserOrderByBalanceDesc(user);
+    }
+
+    public Asset getByUserAndCurrency(User user, Currency currency) {
+        return assetRepository.getAssetByUserAndCurrency(user, currency);
+    }
+
+    public List<Asset> getAllByUser(User user) {
+        return assetRepository.getAllByUser(user);
+    }
+
+    public List<Asset> getAllByCurrency(Currency currency) {
+        return assetRepository.getAllByCurrency(currency);
+    }
+
+    public Asset getById(Integer id) {
+        return assetRepository.findById(id).get();
     }
 }
