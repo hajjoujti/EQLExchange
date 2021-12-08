5ef3c238bedf7cb2ba9129b976bcf5aab7f58ef4
hajjoujti <80820935+hajjoujti@users.noreply.github.com>
2021-12-08 11:19:42 +0100

    Created findByEmail method

--- src/main/java/fr/eql/al36/spring/projet/eqlexchange/repository/UserRepository.java
+++ src/main/java/fr/eql/al36/spring/projet/eqlexchange/repository/UserRepository.java
@@ -3,5 +3,10 @@ package fr.eql.al36.spring.projet.eqlexchange.repository;
 import fr.eql.al36.spring.projet.eqlexchange.domain.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends CrudRepository<User, Integer> {
+
+    Optional<User> findByEmail(String email);
+
 }
