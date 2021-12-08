901122e9235676e8e9ca0ae6696a633e8cff761a
valvenom <valentin.venom@protonmail.com>
2021-12-08 17:39:17 +0100

    Adapted to new data model

--- src/main/java/fr/eql/al36/spring/projet/eqlexchange/repository/TradeOrderRepository.java
+++ src/main/java/fr/eql/al36/spring/projet/eqlexchange/repository/TradeOrderRepository.java
@@ -3,6 +3,7 @@ package fr.eql.al36.spring.projet.eqlexchange.repository;
 import fr.eql.al36.spring.projet.eqlexchange.domain.Asset;
 import fr.eql.al36.spring.projet.eqlexchange.domain.Currency;
 import fr.eql.al36.spring.projet.eqlexchange.domain.TradeOrder;
+import fr.eql.al36.spring.projet.eqlexchange.domain.User;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -12,7 +13,7 @@ public interface TradeOrderRepository extends CrudRepository<TradeOrder, Integer
 
     List<TradeOrder> getAllByAssetIn(List<Asset> assets);
 
-    @Query("SELECT to FROM TradeOrder to WHERE to.currency = :asset_currency AND to.asset.currency = :wanted_currency AND to.completionDate IS NULL AND to.cancellationDate IS NULL ORDER by to.amount")
-    List<TradeOrder> findAllMatchingTradeOrders(@Param("asset_currency") Currency assetCurrency, @Param("wanted_currency") Currency wantedCurrency);
+    @Query("SELECT to FROM TradeOrder to WHERE to.currencyToBuy = :currencyToSell AND to.currencyToSell = :currencyToBuy AND to.completionDate IS NULL AND to.cancellationDate IS NULL AND to.user <> :user ORDER by to.amountToBuy")
+    List<TradeOrder> findAllMatchingTradeOrders(@Param("user") User user, @Param("currency_to_sell") Currency currencyToSell, @Param("wanted_currency") Currency currencyToBuy);
 
 }
