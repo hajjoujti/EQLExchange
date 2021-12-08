b735eb14114c1d8f10a80f044d4f239528fd00c9
hajjoujti <80820935+hajjoujti@users.noreply.github.com>
2021-12-08 11:13:25 +0100

    added Authority attribute

--- src/main/java/fr/eql/al36/spring/projet/eqlexchange/domain/User.java
+++ src/main/java/fr/eql/al36/spring/projet/eqlexchange/domain/User.java
@@ -8,7 +8,10 @@ import java.util.Objects;
 import java.util.Set;
 
 @Entity
-@NoArgsConstructor @AllArgsConstructor @Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 @Builder
 public class User {
 
@@ -17,26 +20,45 @@ public class User {
     private Integer id;
 
     private String firstName;
+
     private String lastName;
+
     private String username;
+
     private LocalDate dateOfBirth;
+
     private String email;
+
     private String password;
+
     private String walletAddress;
 
     @OneToMany(mappedBy = "user")
     Set<Asset> assets;
 
+    @ManyToMany(cascade = CascadeType.MERGE)
+    @JoinTable(name = "user_authority", joinColumns = {
+            @JoinColumn(name = "user_id", referencedColumnName = "id")}, inverseJoinColumns = {
+            @JoinColumn(name = "authority_id", referencedColumnName = "id")})
+    private Set<Authority> authorities;
+
+
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if(this == o) return true;
+        if(o == null || getClass() != o.getClass()) return false;
         User user = (User) o;
-        return Objects.equals(id, user.id) && Objects.equals(firstName, user.firstName) && Objects.equals(lastName, user.lastName) && Objects.equals(username, user.username) && Objects.equals(dateOfBirth, user.dateOfBirth) && Objects.equals(email, user.email) && Objects.equals(password, user.password) && Objects.equals(walletAddress, user.walletAddress);
+        return Objects.equals(id, user.id) && Objects.equals(firstName, user.firstName) && Objects.equals(lastName,
+                                                                                                          user.lastName) &&
+               Objects.equals(username, user.username) && Objects.equals(dateOfBirth, user.dateOfBirth) &&
+               Objects.equals(email, user.email) && Objects.equals(password, user.password) && Objects.equals(
+                walletAddress, user.walletAddress);
     }
+
 
     @Override
     public int hashCode() {
         return Objects.hash(id, firstName, lastName, username, dateOfBirth, email, password, walletAddress);
     }
+
 }
