adf5ac438b547a0b30f4e3966a470a989189513a
valvenom <valentin.venom@protonmail.com>
2021-12-09 13:08:00 +0100

    Added method to get currencies by type

--- src/main/java/fr/eql/al36/spring/projet/eqlexchange/repository/CurrencyRepository.java
+++ src/main/java/fr/eql/al36/spring/projet/eqlexchange/repository/CurrencyRepository.java
@@ -1,6 +1,7 @@
 package fr.eql.al36.spring.projet.eqlexchange.repository;
 
 import fr.eql.al36.spring.projet.eqlexchange.domain.Currency;
+import fr.eql.al36.spring.projet.eqlexchange.domain.CurrencyType;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -11,4 +12,6 @@ public interface CurrencyRepository extends CrudRepository<Currency, Integer> {
 
     @Query("SELECT c FROM Currency c WHERE c.id <> :id ORDER BY c.ticker ASC")
     List<Currency> getAllExceptOneWithId(@Param("id") Integer id);
+
+    List<Currency> getCurrenciesByCurrencyTypeOrderByNameAsc(CurrencyType currencyType);
 }
