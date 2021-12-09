2150eeeca82b0861e2971a930e1d5fa1181d0f3a
hajjoujti <80820935+hajjoujti@users.noreply.github.com>
2021-12-09 12:23:20 +0100

    Corrected commented method

--- src/main/java/fr/eql/al36/spring/projet/eqlexchange/repository/TradeOrderRepository.java
+++ src/main/java/fr/eql/al36/spring/projet/eqlexchange/repository/TradeOrderRepository.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 public interface TradeOrderRepository extends CrudRepository<TradeOrder, Integer> {
 
-    //List<TradeOrder> getAllByAssetIn(List<Asset> assets);
+    List<TradeOrder> findAllByUser(User user);
 
     @Query("SELECT to FROM TradeOrder to WHERE to.currencyToBuy = :currency_to_sell AND to.currencyToSell = :currency_to_buy AND to.completionDate IS NULL AND to.cancellationDate IS NULL AND to.user <> :user ORDER by to.amountToBuy")
     List<TradeOrder> findAllMatchingTradeOrders(@Param("user") User user, @Param("currency_to_buy") Currency currencyToBuy, @Param("currency_to_sell") Currency currencyToSell);
