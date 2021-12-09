575de9454f7c97f6a19c6f9af8c2d1270c12e8f3
valvenom <valentin.venom@protonmail.com>
2021-12-09 13:13:49 +0100

    Added refill mapping

--- src/main/java/fr/eql/al36/spring/projet/eqlexchange/controller/TransactionController.java
+++ src/main/java/fr/eql/al36/spring/projet/eqlexchange/controller/TransactionController.java
@@ -1,10 +1,14 @@
 package fr.eql.al36.spring.projet.eqlexchange.controller;
 
+import fr.eql.al36.spring.projet.eqlexchange.domain.Payment;
 import fr.eql.al36.spring.projet.eqlexchange.domain.User;
+import fr.eql.al36.spring.projet.eqlexchange.service.CurrencyService;
 import fr.eql.al36.spring.projet.eqlexchange.service.TransactionService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.HttpSession;
 
@@ -12,10 +16,11 @@ import javax.servlet.http.HttpSession;
 public class TransactionController {
 
     private final TransactionService transactionService;
+    private final CurrencyService currencyService;
 
-
-    public TransactionController(TransactionService transactionService) {
+    public TransactionController(TransactionService transactionService, CurrencyService currencyService) {
         this.transactionService = transactionService;
+        this.currencyService = currencyService;
     }
 
 
@@ -24,6 +29,19 @@ public class TransactionController {
         User connectedUser = (User) session.getAttribute("sessionUser");
         //model.addAttribute("transactions", transactionService.getTransactionsDoneByUser(connectedUser));
         return "wallet/history";
+    }
+
+    @GetMapping("wallet/refill")
+    public String refillWallet(Model model, HttpSession session) {
+        model.addAttribute("fiatCurrencies",currencyService.getFiatCurrencies());
+        model.addAttribute("payment", new Payment());
+        return "transaction/fillup";
+    }
+
+    @PostMapping("/wallet/refill/pay")
+    public String payRefill(Model model, @ModelAttribute Payment payment, HttpSession session) {
+
+        return displayWallet(model, session);
     }
 
 }
