07072783f21b1110241e8855b066e108195461ad
hajjoujti <80820935+hajjoujti@users.noreply.github.com>
2021-12-09 12:26:33 +0100

    Created getCurrencyPricesJSON method that returns JSON String of CurrencyPrices list

--- src/main/java/fr/eql/al36/spring/projet/eqlexchange/service/CurrencyPriceService.java
+++ src/main/java/fr/eql/al36/spring/projet/eqlexchange/service/CurrencyPriceService.java
@@ -1,14 +1,19 @@
 package fr.eql.al36.spring.projet.eqlexchange.service;
 
+import com.google.gson.Gson;
 import fr.eql.al36.spring.projet.eqlexchange.domain.Currency;
 import fr.eql.al36.spring.projet.eqlexchange.domain.CurrencyPrice;
+import fr.eql.al36.spring.projet.eqlexchange.dto.CurrencyPriceDTO;
 import fr.eql.al36.spring.projet.eqlexchange.repository.CurrencyPriceRepository;
+import org.springframework.boot.json.GsonJsonParser;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class CurrencyPriceService {
+
     private final CurrencyPriceRepository currencyPriceRepository;
 
 
@@ -17,13 +22,24 @@ public class CurrencyPriceService {
         this.currencyPriceRepository = currencyPriceRepository;
     }
 
-    public CurrencyPrice getLatestPriceOFCurrency(Currency currency){
+
+    public CurrencyPrice getLatestPriceOFCurrency(Currency currency) {
         return currencyPriceRepository.findTopByCurrencyOrderByIdDesc(currency);
 
     }
 
-    public List<CurrencyPrice> getPricesOfCurrency(Currency currency){
-        return currencyPriceRepository.getAllByCurrencyOrderByIdAsc(currency);
+
+    public List<CurrencyPrice> getPricesOfCurrency(Currency currency) {
+        return currencyPriceRepository.getAllByCurrencyOrderByDateTimeAsc(currency);
+    }
+
+
+    public String getCurrencyPricesJSON(Currency currency) {
+        List<CurrencyPriceDTO> currencyPriceDTOS = new ArrayList<>();
+        for(CurrencyPrice currencyPrice : getPricesOfCurrency(currency)) {
+            currencyPriceDTOS.add(new CurrencyPriceDTO(currencyPrice.getPrice(), currencyPrice.getDateTime()));
+        }
+        return new Gson().toJson(currencyPriceDTOS);
     }
 
 }
