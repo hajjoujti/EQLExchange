c9777ba92e505ef29414fadaa5444af4a0e157d0
valvenom <valentin.venom@protonmail.com>
2021-12-09 15:35:50 +0100

    Corrected refill mapping

--- src/main/java/fr/eql/al36/spring/projet/eqlexchange/controller/TransactionController.java
+++ src/main/java/fr/eql/al36/spring/projet/eqlexchange/controller/TransactionController.java
@@ -1,8 +1,11 @@
 package fr.eql.al36.spring.projet.eqlexchange.controller;
 
+import fr.eql.al36.spring.projet.eqlexchange.domain.Asset;
 import fr.eql.al36.spring.projet.eqlexchange.domain.Payment;
 import fr.eql.al36.spring.projet.eqlexchange.domain.User;
+import fr.eql.al36.spring.projet.eqlexchange.service.AssetService;
 import fr.eql.al36.spring.projet.eqlexchange.service.CurrencyService;
+import fr.eql.al36.spring.projet.eqlexchange.service.PaymentService;
 import fr.eql.al36.spring.projet.eqlexchange.service.TransactionService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,23 +20,28 @@ public class TransactionController {
 
     private final TransactionService transactionService;
     private final CurrencyService currencyService;
+    private final PaymentService paymentService;
+    private final AssetService assetService;
 
-    public TransactionController(TransactionService transactionService, CurrencyService currencyService) {
+    public TransactionController(TransactionService transactionService, CurrencyService currencyService, PaymentService paymentService, AssetService assetService) {
         this.transactionService = transactionService;
         this.currencyService = currencyService;
+        this.paymentService = paymentService;
+        this.assetService = assetService;
     }
-
 
     @GetMapping("wallet/history")
     public String displayWallet(Model model, HttpSession session) {
         User connectedUser = (User) session.getAttribute("sessionUser");
-        model.addAttribute("user", connectedUser);
+        model.addAttribute("sessionUser", connectedUser);
         //model.addAttribute("transactions", transactionService.getTransactionsDoneByUser(connectedUser));
         return "wallet/history";
     }
 
     @GetMapping("wallet/refill")
     public String refillWallet(Model model, HttpSession session) {
+        User connectedUser = (User) session.getAttribute("sessionUser");
+        model.addAttribute("sessionUser", connectedUser);
         model.addAttribute("fiatCurrencies",currencyService.getFiatCurrencies());
         model.addAttribute("payment", new Payment());
         return "transaction/fillup";
@@ -41,8 +49,12 @@ public class TransactionController {
 
     @PostMapping("/wallet/refill/pay")
     public String payRefill(Model model, @ModelAttribute Payment payment, HttpSession session) {
+        User connectedUser = (User) session.getAttribute("sessionUser");
+        Asset targetAsset = assetService.getByUserAndCurrency(connectedUser,payment.getCurrency());
+        payment.setAsset(targetAsset);
+        paymentService.execute(payment);
 
-        return displayWallet(model, session);
+        return "redirect:/wallet/";
     }
 
 }
