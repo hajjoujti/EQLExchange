dcee77fcea46ba5ebbf30c8da061ff87132eabe2
valvenom <valentin.venom@protonmail.com>
2021-12-09 15:33:57 +0100

    Added execute method

--- src/main/java/fr/eql/al36/spring/projet/eqlexchange/service/PaymentService.java
+++ src/main/java/fr/eql/al36/spring/projet/eqlexchange/service/PaymentService.java
@@ -1,7 +1,23 @@
 package fr.eql.al36.spring.projet.eqlexchange.service;
 
+import fr.eql.al36.spring.projet.eqlexchange.domain.Payment;
+import fr.eql.al36.spring.projet.eqlexchange.repository.PaymentRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PaymentService {
+
+    private final PaymentRepository paymentRepository;
+    private final TransactionService transactionService;
+
+    public PaymentService(PaymentRepository paymentRepository, TransactionService transactionService) {
+        this.paymentRepository = paymentRepository;
+        this.transactionService = transactionService;
+    }
+
+    public void execute(Payment payment) {
+        transactionService.executeFromPayment(payment);
+        paymentRepository.save(payment);
+    }
+
 }
