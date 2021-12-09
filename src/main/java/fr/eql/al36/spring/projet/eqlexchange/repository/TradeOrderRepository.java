659b1ed46828eaa4ce2cf03c910aa4a28cc5ed2b
valvenom <valentin.venom@protonmail.com>
2021-12-09 13:10:54 +0100

    Commented matching method in debugging process

--- src/main/java/fr/eql/al36/spring/projet/eqlexchange/repository/TradeOrderRepository.java
+++ src/main/java/fr/eql/al36/spring/projet/eqlexchange/repository/TradeOrderRepository.java
@@ -13,7 +13,9 @@ public interface TradeOrderRepository extends CrudRepository<TradeOrder, Integer
 
     //List<TradeOrder> getAllByAssetIn(List<Asset> assets);
 
+    /* following would be faster to process if it selected ANYTHING AT ALL
     @Query("SELECT to FROM TradeOrder to WHERE to.currencyToBuy = :currency_to_sell AND to.currencyToSell = :currency_to_buy AND to.completionDate IS NULL AND to.cancellationDate IS NULL AND to.user <> :user ORDER by to.amountToBuy")
     List<TradeOrder> findAllMatchingTradeOrders(@Param("user") User user, @Param("currency_to_buy") Currency currencyToBuy, @Param("currency_to_sell") Currency currencyToSell);
+    */
 
 }
