de6e77790b8dd6525bafa0fe095a01644ca55623
valvenom <valentin.venom@protonmail.com>
2021-12-10 11:28:17 +0100

    Added random and linear currency price generation methods + save method

--- src/main/java/fr/eql/al36/spring/projet/eqlexchange/service/CurrencyPriceService.java
+++ src/main/java/fr/eql/al36/spring/projet/eqlexchange/service/CurrencyPriceService.java
@@ -8,6 +8,8 @@ import fr.eql.al36.spring.projet.eqlexchange.repository.CurrencyPriceRepository;
 import org.springframework.boot.json.GsonJsonParser;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -42,4 +44,55 @@ public class CurrencyPriceService {
         return new Gson().toJson(currencyPriceDTOS);
     }
 
+    public List<CurrencyPrice> generateRandomCurrencyPrices(Currency currency, LocalDateTime startDateTime, Integer interval, double startPrice) {
+        List<CurrencyPrice> currencyPrices = new ArrayList<>();
+
+        LocalDateTime dateTime = startDateTime;
+        double price = startPrice;
+
+        System.out.println("Generating random prices for currency " + currency.getTicker() + ",starting at " + startPrice + "$ from " + startDateTime + " every " + interval + " seconds");
+        while (dateTime.isBefore(LocalDateTime.now())) {
+            CurrencyPrice currencyPrice = new CurrencyPrice();
+            currencyPrice.setCurrency(currency);
+            double randomNumber = Math.random();
+            double randomPricePart = price * Math.random();
+            if (Math.random() > .5) price += randomPricePart * randomNumber;
+            else price -= randomPricePart * randomNumber;
+            currencyPrice.setPrice(price);
+            currencyPrice.setDateTime(dateTime);
+            currencyPrices.add(currencyPrice);
+            System.out.println(currencyPrice.getCurrency().getTicker() + " price on " + currencyPrice.getDateTime() + " is " + currencyPrice.getPrice() + "$");
+            dateTime = dateTime.plusSeconds(interval);
+        }
+        System.out.println("Stopped generating after " + currencyPrices.size() + " iterations");
+        return currencyPrices;
+    }
+
+    public List<CurrencyPrice> generateLinearCurrencyPrices(Currency currency, LocalDateTime startDateTime, Integer interval) {
+        List<CurrencyPrice> currencyPrices = new ArrayList<>();
+
+        LocalDateTime dateTime = startDateTime;
+        double price = 1;
+
+        System.out.println("Generating linear prices for currency " + currency.getTicker() + " at " + price + "$ from " + startDateTime + " every " + interval + " seconds");
+        while (dateTime.isBefore((LocalDateTime.now()))) {
+            CurrencyPrice currencyPrice = new CurrencyPrice();
+            currencyPrice.setCurrency(currency);
+            currencyPrice.setPrice(price);
+            currencyPrice.setDateTime(dateTime);
+            currencyPrices.add(currencyPrice);
+            dateTime = dateTime.plusSeconds(interval);
+        }
+        System.out.println("Stopped generating after " + currencyPrices.size() + " iterations");
+        return currencyPrices;
+    }
+
+    @Transactional
+    public void saveCurrencyPrices(List<CurrencyPrice> currencyPrices) {
+        for (CurrencyPrice currencyPrice : currencyPrices) {
+            System.out.println("saving " + currencyPrice.getId() + "///" + currencyPrice.getPrice());
+            currencyPrice = currencyPriceRepository.save(currencyPrice);
+            System.out.println("saving " + currencyPrice.getId() + "///" + currencyPrice.getPrice());
+        }
+    }
 }
